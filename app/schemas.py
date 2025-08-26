85fef72ed201fbe1c786b29a6fd7d31d31609618
Pritiks23 <65793273+Pritiks23@users.noreply.github.com>
2025-08-26 09:51:49 -0700

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,10 @@
 from typing import Any, List, Optional
-
+from datetime import datetime
 from pydantic import BaseModel, field_validator
 
-
+# --------------------
+# School Schemas
+# --------------------
 class SchoolPostModel(BaseModel):
     name: str
 
@@ -26,6 +28,9 @@ class SchoolUpdateModel(BaseModel):
     user_accounts: Optional[List[int]] = None
 
 
+# --------------------
+# Classroom Schemas
+# --------------------
 class ClassroomPostModel(BaseModel):
     name: str
     school_id: int
@@ -48,6 +53,9 @@ class ClassroomUpdateModel(BaseModel):
     user_accounts: Optional[List[int]] = None
 
 
+# --------------------
+# UserAccount Schemas
+# --------------------
 class UserAccountPostModel(BaseModel):
     name: str
     email: str
@@ -71,3 +79,35 @@ class UserAccountUpdateModel(BaseModel):
     name: Optional[str] = None
     email: Optional[str] = None
     is_student: Optional[bool] = None
+
+
+# --------------------
+# Assignment Schemas
+# --------------------
+class AssignmentPostModel(BaseModel):
+    title: str
+    body: str
+    submission_date: Optional[datetime] = None
+    classroom_id: int
+    student_id: int
+
+
+class AssignmentModel(AssignmentPostModel):
+    id: int
+    classroom: Optional[int] = None
+    student: Optional[int] = None
+
+    @field_validator("classroom", "student", mode="before")
+    @classmethod
+    def get_id(cls, value: Any) -> Optional[int]:
+        if value:
+            return value.id
+        return None
+
+
+class AssignmentUpdateModel(BaseModel):
+    title: Optional[str] = None
+    body: Optional[str] = None
+    submission_date: Optional[datetime] = None
+    classroom_id: Optional[int] = None
+    student_id: Optional[int] = None
