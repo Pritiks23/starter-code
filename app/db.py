565d8ba43ff8130a178a1b10fa510367fecd01f8
Pritiks23 <65793273+Pritiks23@users.noreply.github.com>
2025-08-26 09:44:28 -0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from datetime import datetime
 from sqlalchemy import Column, ForeignKey, String, Table, create_engine
 from sqlalchemy.orm import (
     DeclarativeBase,
@@ -10,8 +10,8 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 
-engine = create_engine("sqlite:///./app.db", connect_args={"check_same_thread": False})
 
+engine = create_engine("sqlite:///./app.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -34,10 +34,6 @@ classroom_user_account_table = Table(
 
 
 class School(Base):
-    """
-    Represents a School in the system
-    """
-
     __tablename__ = "school"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     name: Mapped[str] = mapped_column(String(255), unique=True)
@@ -46,10 +42,6 @@ class School(Base):
 
 
 class Classroom(Base):
-    """
-    Represents a Classroom in the system
-    """
-
     __tablename__ = "classroom"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     name: Mapped[str] = mapped_column(String(255), unique=True)
@@ -58,13 +50,12 @@ class Classroom(Base):
     user_accounts: Mapped[List["UserAccount"]] = relationship(
         secondary=classroom_user_account_table, back_populates="classrooms"
     )
+    assignments: Mapped[List["Assignment"]] = relationship(
+        back_populates="classroom", cascade="all, delete"
+    )
 
 
 class UserAccount(Base):
-    """
-    Represents a Classroom in the system
-    """
-
     __tablename__ = "user_account"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     name: Mapped[str] = mapped_column(String(255))
@@ -77,7 +68,24 @@ class UserAccount(Base):
     classrooms: Mapped[List["Classroom"]] = relationship(
         secondary=classroom_user_account_table, back_populates="user_accounts"
     )
+    assignments: Mapped[List["Assignment"]] = relationship(
+        back_populates="student", cascade="all, delete"
+    )
 
 
+class Assignment(Base):
+    __tablename__ = "assignment"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    title: Mapped[str] = mapped_column(String(255))
+    body: Mapped[str] = mapped_column(String)  # simple text field
+    submission_date: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
+    classroom_id: Mapped[int] = mapped_column(ForeignKey("classroom.id", ondelete="CASCADE"))
+    classroom: Mapped["Classroom"] = relationship("Classroom", back_populates="assignments")
+
+    student_id: Mapped[int] = mapped_column(ForeignKey("user_account.id", ondelete="CASCADE"))
+    student: Mapped["UserAccount"] = relationship("UserAccount", back_populates="assignments")
+
+# Configure mappers
 Base.registry.configure()
 configure_mappers()
